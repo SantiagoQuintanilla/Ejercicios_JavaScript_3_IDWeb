627c36eb71da1b60f2d72c57d1a922df808eb6a6
SantiagoQuintanilla <squintanilla@unsa.edu.pe>
2025-10-28 12:47:56 -0500

    Se añade Ejercicio01

---
+++ Ejercicio01/script.js
@@ -0,0 +1,7 @@
+const SeleccionPorId=document.getElementById('primer');
+const SeleccionPorEtiqueta=document.getElementsByTagName('p');
+const SeleccionPorClase=document.getElementsByClassName('parrafo');
+
+console.log('Selección por id: ', SeleccionPorId); 
+console.log('Selección por etiqueta: ', SeleccionPorEtiqueta);
+console.log('Selección por clase: ', SeleccionPorClase); 
