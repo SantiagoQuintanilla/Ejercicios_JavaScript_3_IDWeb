1249f607313d050a512395b480d7732748607686
SantiagoQuintanilla <squintanilla@unsa.edu.pe>
2025-10-30 20:58:41 -0500

    Finalizado Ejercicio 3

--- Ejercicio03/script.js
+++ Ejercicio03/script.js
@@ -1,11 +1,32 @@
-const ul=document.getElementById("lista");
-["uno", "dos", "tres"].forEach(text => {
-    const li=document.createElement("li"); 
-    li.textContent=text; 
-    ul.appendChild(li); 
-}); 
-const li4=document.createElement("li"); 
-li4.textContent="cuatro";
-ul.appendChild(li4); 
-const segundo=ul.children[1]; 
-if (segundo) segundo.remove();
+const lista=document.createElement("ul");
+
+const item1=document.createElement("li");
+item1.textContent="Elemento N°1";
+lista.appendChild(item1);
+
+const item2=document.createElement("li");
+item2.textContent="Elemento N°2";
+lista.appendChild(item2);
+
+const item3=document.createElement("li");
+item3.textContent="Elemento N°3";
+lista.appendChild(item3);
+
+document.body.appendChild(lista);
+
+console.log("Lista original:");
+for (let li of lista.children) {
+  console.log(li.textContent);
+}
+
+const nuevoItem4=document.createElement("li");
+nuevoItem4.textContent="Elemento N°4 (agregado dinámicamente con JS";
+lista.appendChild(nuevoItem4);
+
+const elemento2=lista.children[1];
+lista.removeChild(elemento2);
+
+console.log("Lista modificada:");
+for (let li of lista.children) {
+  console.log(li.textContent);
+}
