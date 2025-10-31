4f0b7d0120c9902fbf5ae356521c87f8961a8ff1
SantiagoQuintanilla <squintanilla@unsa.edu.pe>
2025-10-30 21:41:51 -0500

    Finalizado Ejercicio 7

---
+++ Ejercicio07/script.js
@@ -0,0 +1,43 @@
+const form=document.createElement("form");
+
+const input=document.createElement("input");
+input.type="text";
+input.placeholder="Escribir tarea";
+
+const botonAgregar=document.createElement("button");
+botonAgregar.textContent="Agregar tarea";
+
+form.appendChild(input);
+form.appendChild(botonAgregar);
+
+const lista=document.createElement("ul");
+
+document.body.appendChild(form);
+document.body.appendChild(lista);
+
+form.addEventListener("submit", (e) => {
+
+    e.preventDefault();
+
+    const textoTarea=input.value;
+
+    const li=document.createElement("li");
+    li.textContent=textoTarea;
+
+    const botonEliminar=document.createElement("button");
+    botonEliminar.textContent="Eliminar tarea";
+    botonEliminar.classList.add("Boton_Eliminar");
+    botonEliminar.style.marginLeft="10px";
+
+    li.appendChild(botonEliminar);
+    lista.appendChild(li);
+
+    input.value="";
+});
+
+lista.addEventListener("click", (e) => {
+    if (e.target.classList.contains("Boton_Eliminar")) {
+        const li=e.target.parentElement;
+        lista.removeChild(li);
+    }
+})
